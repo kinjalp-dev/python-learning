48fd1e793c4dd70b67c7678676c5f5c64b1ae5aa
Kinjal Parmar <kinjalparmar.dev@gmail.com>
2026-01-08 21:50:28 +0530

    Assignment 1

---
+++ task1.py
@@ -0,0 +1,19 @@
+# mathematical operations on these two numbers
+
+number1 =  int(input("Enter the first number: "))
+number2 =  int(input("Enter the second number: "))
+
+print("\n")
+# Addition
+addition = number1 + number2
+print("Addition:", addition)
+# Subtraction
+subtraction = number1 - number2
+print("Subtraction:", subtraction)
+# Multiplication
+multiplication = number1 * number2
+print("Multiplication:", multiplication)
+# Division
+if number2 != 0:
+    division = number1 / number2
+    print("Division:", division)
---
+++ task2.py
@@ -0,0 +1,7 @@
+# personalized greeting message
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+message = "Hello," + " " + first_name + " " + last_name + "! Welcome to the Python program."
+print("\n"+ message)
